187ebe9bc11d306d4fe05720e388cefb72061a58
XamitlHernandez <xamitlhernandez@gmail.com>
2024-11-27 20:51:10 -0600

    Actualizacion de Fabio

--- Raices-Ecommerce/src/pages/Formulario/Compradores/formulario2.js
+++ Raices-Ecommerce/src/pages/Formulario/Compradores/formulario2.js
@@ -26,7 +26,7 @@
     const usuarioJSON = JSON.stringify(usuario, null, 2); // Formateado para mejor visualización (opcional)
     
     // Mostrar el JSON en la consola para pruebas
-    console.log("Usuario generado:", usuarioJSON);
+    console.log(usuarioJSON);
 
     return usuarioJSON; // Devolver el JSON por si necesitas usarlo en otro lugar
 }
@@ -47,8 +47,11 @@ function validarFormulario() {
     const telefonoRegex = /^[0-9]{10,}$/;
     const codigoPostalRegex = /^[0-9]{5}$/;
 
+
     // Validar nombre
     if (nombre === "") {
+        document.getElementById("nombre").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("nombre").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Campo vacío",
@@ -56,6 +59,8 @@ function validarFormulario() {
         });
         return false;
     } else if (!soloLetrasRegex.test(nombre)) {
+        document.getElementById("nombre").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("nombre").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -66,6 +71,8 @@ function validarFormulario() {
 
     // Validar apellido
     if (apellido === "") {
+        document.getElementById("apellido").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("apellido").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Campo vacío",
@@ -73,6 +80,8 @@ function validarFormulario() {
         });
         return false;
     } else if (!soloLetrasRegex.test(apellido)) {
+        document.getElementById("apellido").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("apellido").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -83,6 +92,8 @@ function validarFormulario() {
 
     // Validar email
     if (email === "") {
+        document.getElementById("email").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("email").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Campo vacío",
@@ -90,6 +101,8 @@ function validarFormulario() {
         });
         return false;
     } else if (!emailRegex.test(email)) {
+        document.getElementById("email").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("email").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -100,6 +113,8 @@ function validarFormulario() {
 
     // Validar contraseña
     if (password.length < 8) {
+        document.getElementById("password").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("password").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Contraseña insegura",
@@ -110,6 +125,8 @@ function validarFormulario() {
 
     // Validar dirección
     if (direccion === "") {
+        document.getElementById("direccion").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("direccion").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Campo vacío",
@@ -117,6 +134,8 @@ function validarFormulario() {
         });
         return false;
     } else if (!direccionRegex.test(direccion)) {
+        document.getElementById("direccion").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("direccion").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -127,6 +146,8 @@ function validarFormulario() {
 
     // Validar estado
     if (estadoSeleccionado === "Elige") {
+        document.getElementById("inputState").className="alert alert-danger pt-2 pb-2";
+        document.getElementById("inputState").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Estado no seleccionado",
@@ -137,6 +158,8 @@ function validarFormulario() {
 
     // Validar C.P
     if (!codigoPostalRegex.test(codigoPostal)) {
+        document.getElementById("cp").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("cp").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -147,6 +170,8 @@ function validarFormulario() {
 
     // Validar teléfono
     if (telefono === "") {
+        document.getElementById("telefono").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("telefono").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Campo vacío",
@@ -154,6 +179,8 @@ function validarFormulario() {
         });
         return false;
     } else if (!telefonoRegex.test(telefono)) {
+        document.getElementById("telefono").className="form-control alert alert-danger pt-2 pb-2";
+        document.getElementById("telefono").setAttribute("role", "alert");
         Swal.fire({
             icon: "error",
             title: "Formato incorrecto",
@@ -170,10 +197,10 @@ function validarFormulario() {
             text: "Por favor, marca la casilla para continuar.",
         });
         return false;
-    }
+    } else {
 
     // Si todas las validaciones pasan
-    const usuarioJSON = generarObjetoUsuario();
+    generarObjetoUsuario();
 
     Swal.fire({
         icon: "success",
@@ -186,8 +213,13 @@ function validarFormulario() {
 
     return true;
 }
-generarObjetoUsuario()
+}
 
+
+document.getElementById("registro").addEventListener("click",(evento)=>{
+    evento.preventDefault();
+   validarFormulario();
+})
 
 // Para validar solo que seleccione un estado de la lista
 document.addEventListener("DOMContentLoaded", () => {
